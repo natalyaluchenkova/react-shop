4fe5f44844a3b9d36131eaa78691c5bbe7429f1a
Natalya <luchenkova.natalya@gmail.com>
2022-01-24 14:55:45 +0200

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className='grey darken-2'>
       <div className='nav-wrapper'>
-        <a href='!#' className='brand-logo'>
+        <a href='/' className='brand-logo'>
           React Shop
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='!#'>Repo</a>
+            <a
+              href='https://github.com/natalyaluchenkova/search-movies'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
