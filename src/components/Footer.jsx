beedf30e663f0986abf5c1349f08f1a1905d8a23
Natalya <luchenkova.natalya@gmail.com>
2022-01-24 14:57:43 +0200

    correct link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           © {new Date().getFullYear()} Copyright Text
           <a
             className='grey-text text-lighten-4 right'
-            href='https://github.com/natalyaluchenkova/search-movies'
+            href='https://github.com/natalyaluchenkova/react-shop'
             target='_blank'
             rel='noreferrer'
           >
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
             <a
-              href='https://github.com/natalyaluchenkova/search-movies'
+              href='https://github.com/natalyaluchenkova/react-shop'
               target='_blank'
               rel='noreferrer'
             >
